03df50488d920a37ee370facbc8b62aacdd92f05
ShubhamKumarJha <shubhamkumarjha40@gmail.com>
2024-11-02 17:44:55 +0530

    Initial Commit

---
+++ src/main/java/com/nexo/springfeatures/SpringfeaturesApplication.java
@@ -0,0 +1,13 @@
+package com.nexo.springfeatures;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringfeaturesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringfeaturesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nexo/springfeatures/SpringfeaturesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nexo.springfeatures;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringfeaturesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
